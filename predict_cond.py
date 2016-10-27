7ebaa4fc4b63d68685d27abbafa309f2dfecae9e
jesselangdon <jesselangdon@gmail.com>
2016-10-27 13:29:49 -0700

    Added metadata function.  Added docstrings.  Added clear in-memory function.

--- predict_cond.py
+++ predict_cond.py
@@ -1,22 +1,34 @@
 # file name:	predict_cond.py
 # description:	This tool automates the process of predicting conductivity values for a stream network. Based on a table
 #               of summarized model parameters (output from the Pre-process Environmental Parameters tool) , a Random Forest
-#               model is applied to the parameter table using an external R script.  After R script is called, the RF
-#               prediction is then joined back to the input stream network.
+#               (RF) model is applied to the parameter table using an external R script.  After the R script is called, the
+#               RF prediction is then joined back to the input stream network.
 # author:		Jesse Langdon
 # dependencies: ESRI arcpy module, built-in Python modules
-# version:		0.1
+# version:		0.2
 
 import arcpy
 import subprocess
 import os.path
+import sys
 import gc
+import metadata
 
 inFC = arcpy.GetParameterAsText(0) # stream network polyline feature class (i.e. segments)
 inParams = arcpy.GetParameterAsText(1) # filepath to the dbf file with summarized parameters ( i.e. ws_cond_param.dbf)
 outFC = arcpy.GetParameterAsText(2) # stream network polyline feature class, with predicted conductivity
+outMeta = arcpy.GetParameterAsText(3) # metadata XML file
 
 def checkLineOID(inFC):
+    """Checks the input upstream catchment area polygon feature class for the
+    presence of an attribute field named 'LineOID'.
+
+    Args:
+        inFC: Input upstream catchment area polygon feature class
+
+    Returns:
+        A boolean true or false value.
+    """
     fieldName = "LineOID"
     fields = arcpy.ListFields(inFC, fieldName)
     for field in fields:
@@ -25,28 +37,46 @@ def checkLineOID(inFC):
         else:
             return False
 
-def main(inFC, inParams, outFC):
+
+def main(inFC, inParams, outFC, outMeta):
+    """Main processing function for the Predict Conductivity tool.
+
+    Args:
+        inFC: Input stream network polyline feature class
+        inParams: 2D list of model parameter names and associated raster
+        dataset names
+        outFC: Output stream network polyline feature class, with predicted
+        conductivity values joined as new attribute fields.
+    """
+
     if checkLineOID(inFC) == True:
         arcpy.AddMessage("Predicting conductivity using Random Forest model in R...")
         gc.enable() # turn on automatic garbage collection
         outDir = os.path.dirname(outFC) # get output directory path
 
+        # start writing metadata
+        mWriter = metadata.MetadataWriter("Predict Conductivity Tool", "0.2")
+        mWriter.createRun()
+        mWriter.currentRun.addParameter("Stream Network Polyline Feature Class", inFC)
+        mWriter.currentRun.addParameter("Environmental Parameter Table", inParams)
+        mWriter.currentRun.addOutput("Predicted Conductivity Output Feature Class", outFC)
+
         # variables for the subprocess function
-        pathName = os.path.dirname(os.path.abspath(__file__))
+        pathName = os.path.dirname(os.path.abspath(__file__)) # filepath for the predict_cond.py file
         scriptName = 'condRF.R'
         modelName = 'rf17bCnd9.rdata'
         scriptPath = os.path.join(pathName, scriptName)
         modelPath = os.path.join(pathName, modelName)
 
-        argR = [modelPath, outDir, inParams] # list of arguments that will be passed to condRF.R script
+        argR = [modelPath, outDir, inParams] # list of arguments for condRF.R script
 
-        cmd = ['Rscript', scriptPath] + argR # construct the R command line argument
+        cmd = ['Rscript', scriptPath] + argR # construct R command line argument
 
-        # send the command to the predict_conductivity.r script
+        # send command to predict_conductivity.r
         process = subprocess.Popen(cmd, universal_newlines=True, shell=True)
         process.wait()
 
-        # the predictive output as a csv, for joining to the stream segments feature class
+        # predictive output
         predictedCondCSV = outDir + "\\predicted_cond.csv"
         arcpy.TableToTable_conversion(predictedCondCSV, outDir, r"predicted_cond.dbf")
 
@@ -63,9 +93,12 @@ def main(inFC, inParams, outFC):
         # clean up temporary files
         arcpy.Delete_management(outDir + r"\predicted_cond.dbf")
         arcpy.Delete_management(outDir + r"\predicted_cond.csv")
-
-        # turn off automatic garbage collection
         gc.disable()
+
+        # finalize and write metadata file
+        strToolStatus = "Success"
+        mWriter.finalizeRun(strToolStatus)
+        mWriter.writeMetadataFile(outMeta)
 
         arcpy.AddMessage("Conductivity prediction process complete!")
     else:
@@ -73,5 +106,6 @@ def main(inFC, inParams, outFC):
         sys.exit(0) # terminate process
     return
 
+
 if __name__ == "__main__":
-    main(inFC, inParams, outFC)
+    main(inFC, inParams, outFC, outMeta)
